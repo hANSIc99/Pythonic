e399c1b803fd60be16d2b4e7d224c62caf970902
Stephan Avenwedde <s.avenwedde@gmail.com>
2018-08-19 21:44:39 +0200

    Preparing python wheel

---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open('README.whl', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name='Pythonic',
+        version='0.6',
+        author='Example_author',
+        author_email='s.avenwedde@gmail.com',
+        description='A Python based trading platform for digital currencies',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/hANSIc99/Pythonics',
+        package_dir = {'' : 'src'},
+        python_requires = '>=3.5',
+        install_requires = [
+            'PyQt5>=5.6,<=5.8.2',
+            'pandas>=0.20.3',
+            'python-binance>=0.5.16'],
+        classifiers = [
+            'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Financial and Insurance Industry',
+            'Intended Audience :: End Users/Desktop',
+            'Intended Audience :: Education',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Operating System :: OS Independent',
+            'Environment :: X11 Applications :: Qt',
+            'Natural Language :: German',
+            'Natural Language :: English',
+            'Natural Language :: Chinese (Simplified)',
+            'Natural Language :: Spanish',
+            'Topic :: Office/Business :: Financial'
+            ],
+        )
