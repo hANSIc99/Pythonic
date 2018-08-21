e55c4f05e533972ad145d2eb18854bc6c41d77ad
Stephan Avenwedde <s.avenwedde@gmail.com>
2018-08-21 10:48:44 +0200

    Pythonic start script done

--- src/Pythonic/script.py
+++ src/Pythonic/script.py
@@ -1,11 +1,9 @@
 #!/usr/local/bin/python3
 
 import sys, os, Pythonic 
+from subprocess import Popen
 
 def run():
     print('Python starts at {}'.format(sys.prefix))
-    os.chdir(os.path.dirname(Pythonic.__file__))
-    print('Folder changed to {}'.format(os.getcwd()))
-    from Pythonic import main
-    main.run()
+    Popen(['python3', 'main.py'], cwd= os.path.dirname(Pythonic.__file__))
 
