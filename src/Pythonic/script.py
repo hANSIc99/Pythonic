a1a7e79fdb812b602fc7c8432f8865508b9c474a
Stephan Avenwedde <s.avenwedde@gmail.com>
2018-12-02 15:39:44 +0100

    Release 0.3 created

--- src/Pythonic/script.py
+++ src/Pythonic/script.py
@@ -4,6 +4,8 @@ import sys, os, Pythonic
 from subprocess import Popen
 
 def run():
-    print('Python starts at {}'.format(sys.prefix))
-    Popen(['python3', 'main.py'], cwd= os.path.dirname(Pythonic.__file__))
+    if os.name == 'nt':
+        Popen(['python', 'main.py'], cwd= os.path.dirname(Pythonic.__file__))
+    else:
+        Popen(['python3', 'main.py'], cwd= os.path.dirname(Pythonic.__file__))
 
