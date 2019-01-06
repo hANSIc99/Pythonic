38b9fcf7a47c94119cdf099de92cfa0522e857d1
Stephan Avenwedde <s.avenwedde@gmail.com>
2019-01-06 14:18:41 +0100

    dev progress on stack element

--- src/Pythonic/elements/basic_stack.py
+++ src/Pythonic/elements/basic_stack.py
@@ -1,7 +1,7 @@
 from elementmaster import ElementMaster, alphabet
 from PyQt5.QtCore import Qt, QCoreApplication, pyqtSignal, QVariant
 from PyQt5.QtGui import  QPixmap, QPainter, QColor
-from PyQt5.QtWidgets import (QVBoxLayout, QHBoxLayout, QLabel, QTextEdit, QWidget, QComboBox, QCheckBox, QFileDialog, QPushButton)
+from PyQt5.QtWidgets import (QVBoxLayout, QHBoxLayout, QLabel, QTextEdit, QWidget, QComboBox, QCheckBox, QFileDialog, QPushButton, QStackedWidget)
 from record_function import Record, Function
 from elementeditor import ElementEditor
 from PyQt5.QtCore import QCoreApplication as QC
@@ -20,8 +20,8 @@ class ExecStack(ElementMaster):
         self.row = row
         self.column = column
 
-        # currentdata, currentindex, ischecked
-        self.config = (None, None, False)
+        # filename, log_state 
+        self.config = (None, False)
         super().__init__(self.row, self.column, QPixmap(self.pixmap_path), True, self.config)
         super().edit_sig.connect(self.edit)
         logging.debug('ExecStack called at row {}, column {}'.format(row, column))
@@ -44,6 +44,9 @@ class ExecStack(ElementMaster):
 
     def edit(self):
         logging.debug('edit() called ExecStack')
+
+        self.filename, log_state = self.config
+
         self.returnEditLayout = QVBoxLayout()
 
         self.returnEdit = ElementEditor(self)
@@ -52,18 +55,41 @@ class ExecStack(ElementMaster):
         self.top_text = QLabel()
         self.top_text.setText(QC.translate('', 'Stack ...'))
 
+        self.filename_text = QLabel()
+        if self.filename:
+            self.filename_text.setText(self.filename)
+        else:
+            self.filename_text.setText(QC.translate('', 'Filename'))
+
         self.file_button = QPushButton(QC.translate('', 'Choose file'))
         self.file_button.clicked.connect(self.ChooseFileDialog)
+
+        self.variable_box = QStackedWidget()
+        self.writeInput()
+        self.readInput()
+
+        self.mode_text = QLabel()
+        self.mode_text.setText(QC.translate('', 'Select mode:'))
+
+        self.select_mode = QComboBox()
+        self.select_mode.addItem(QC.translate('', 'Write'), QVariant('w'))
+        self.select_mode.addItem(QC.translate('', 'Read'), QVariant('r'))
 
         self.help_text = QWidget()
         self.help_text_layout = QVBoxLayout(self.help_text)
 
+        # Option: Fixed size
         self.help_text_1 = QLabel()
-        self.help_text_1.setText(QC.translate('', 'Help Text 1')) 
+        # Liste
+        self.help_text_1.setText(QC.translate('', 'On input: Write / Read')) 
 
+
+        # Input: Liste
         self.help_text_2 = QLabel()
-        self.help_text_2.setText(QC.translate('', 'Help Text 2'))
+        self.help_text_2.setText(QC.translate('', 'Insert /Append'))
 
+        # Option: Remove output
+        # Output: Liste: First Out /  Last Out / all Out 
         self.help_text_3 = QLabel()
         self.help_text_3.setText(QC.translate('', 'Help Text 3'))
 
@@ -88,15 +114,18 @@ class ExecStack(ElementMaster):
         self.element_selector = QComboBox()
         self.populateSelector()
 
-        if self.config[1]:
-            self.element_selector.setCurrentIndex(self.config[1])
-        if self.config[2]:
+        if log_state:
             self.log_checkbox.setChecked(True)
 
         self.confirm_button.clicked.connect(self.returnEdit.closeEvent)
+        self.select_mode.currentIndexChanged.connect(self.indexChanged)
         self.returnEdit.window_closed.connect(self.edit_done)
         self.returnEditLayout.addWidget(self.top_text)
+        self.returnEditLayout.addWidget(self.filename_text)
         self.returnEditLayout.addWidget(self.file_button)
+        self.returnEditLayout.addWidget(self.mode_text)
+        self.returnEditLayout.addWidget(self.select_mode)
+        self.returnEditLayout.addWidget(self.variable_box)
         self.returnEditLayout.addWidget(self.help_text)
         self.returnEditLayout.addStretch(1)
         self.returnEditLayout.addWidget(self.log_line)
@@ -111,7 +140,52 @@ class ExecStack(ElementMaster):
         if fileName:
             logging.debug('ChooseFileDialog() called with filename: {}'.format(fileName))
             self.filename = fileName
-            self.save_file.emit(fileName)
+            self.filename_text.setText(self.filename)
+
+    def writeInput(self):
+
+        self.write_input = QWidget()
+        self.write_layout = QHBoxLayout(self.write_input)
+
+        self.write_txt = QLabel()
+        self.write_txt.setText(QC.translate('', 'Do this with input:'))
+
+        self.select_write_mode = QComboBox()
+        self.select_write_mode.addItem(QC.translate('', 'Insert'), QVariant('i'))
+        self.select_write_mode.addItem(QC.translate('', 'Append'), QVariant('a'))
+
+
+        self.write_layout.addWidget(self.write_txt)
+        self.write_layout.addWidget(self.select_write_mode)
+
+
+        self.variable_box.addWidget(self.write_input)
+
+    def readInput(self):
+
+        self.read_input = QWidget()
+        self.read_layout = QHBoxLayout(self.read_input)
+
+        self.read_txt = QLabel()
+        self.read_txt.setText(QC.translate('', 'Do this when triggered:'))
+
+        self.select_read_mode = QComboBox()
+        self.select_read_mode.addItem(QC.translate('', 'First out'), QVariant('f'))
+        self.select_read_mode.addItem(QC.translate('', 'Last out'), QVariant('l'))
+        self.select_read_mode.addItem(QC.translate('', 'All out'), QVariant('a'))
+
+
+        self.read_layout.addWidget(self.read_txt)
+        self.read_layout.addWidget(self.select_read_mode)
+
+
+        self.variable_box.addWidget(self.read_input)
+
+    def indexChanged(self, event):
+
+        current_index = event
+        logging.debug('indexChanged() called {}'.format(current_index))
+        self.variable_box.setCurrentIndex(current_index)
 
 
     def populateSelector(self):
@@ -125,8 +199,9 @@ class ExecStack(ElementMaster):
 
     def edit_done(self):
         logging.debug('edit_done() called ExecStack' )
-        self.config = (self.element_selector.currentData(), self.element_selector.currentIndex(),
-                self.log_checkbox.isChecked())
+        log_state = self.log_checkbox.isChecked()
+        filename = self.filename
+        self.config = (filename, log_state)
         self.addFunction(StackFunction)
 
 class StackFunction(Function):
