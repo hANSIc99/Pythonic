b5c45bdaf02bb7c2cca3a03c8f2564af835f1115
Stephan Avenwedde <s.avenwedde@gmail.com>
2019-02-17 21:52:37 +0100

    working on basic stack implementation

---
+++ src/Pythonic/elements/basic_stack_window.py
@@ -0,0 +1,60 @@
+from PyQt5.QtWidgets import (QLabel, QWidget, QVBoxLayout, QHBoxLayout, QSizePolicy, QStyleOption, QStyle,
+                                QPushButton, QTextEdit, QMainWindow)
+from PyQt5.QtGui import QPixmap, QPainter, QPen, QFont
+from PyQt5.QtCore import Qt, QCoreApplication, pyqtSignal, QPoint, QRect
+from PyQt5.QtCore import QCoreApplication as QC
+from PyQt5.QtCore import QThread
+from elementmaster import alphabet
+import multiprocessing as mp
+from elementeditor import ElementEditor
+import logging, sys, time, traceback
+
+class StackWindow(QWidget):
+
+    def __init__(self, parent):
+
+        super().__init__(parent)
+        self.parent = parent
+        self.setAttribute(Qt.WA_DeleteOnClose, True)
+
+    def raiseWindow(self):
+
+        logging.debug('StackWindow() called')
+        self.setMinimumSize(400, 300)
+        self.setWindowFlags(Qt.Window)
+        self.setWindowTitle(QC.translate('', 'Stack'))
+        self.setWindowModality(Qt.WindowModal)
+
+        self.confirm_button = QPushButton()
+        self.confirm_button.setText(QC.translate('', 'Ok'))
+        self.confirm_button.clicked.connect(self.close)
+        # BAUSTELLE
+        #self.confirm_button.clicked.connect(self.parent.toggle_debug)
+        #self.confirm_button.clicked.connect(self.parent.icon_bar.mousePressEvent) 
+
+        self.headline = QFont("Arial", 10, QFont.Bold)
+
+        self.info_string = QC.translate('', 'Debug info of element:')
+        self.elementInfo = QLabel()
+        self.elementInfo.setFont(self.headline)
+        #self.elementInfo.setText(self.info_string + '{} {}'.format(self.source[0],
+            #alphabet[self.source[1]]))
+        self.elementInfo.setText('test 123')
+
+        self.debugMessage = QTextEdit()
+        self.debugMessage.setReadOnly(True)
+        self.debugMessage.setText('debugMessage')
+
+
+
+        self.debugWindowLayout = QVBoxLayout()
+        self.debugWindowLayout.addWidget(self.elementInfo)
+        self.debugWindowLayout.addWidget(self.debugMessage)
+        self.debugWindowLayout.addStretch(1)
+        self.debugWindowLayout.addWidget(self.confirm_button)
+
+        self.setLayout(self.debugWindowLayout)   
+        
+        self.show()
+
+
