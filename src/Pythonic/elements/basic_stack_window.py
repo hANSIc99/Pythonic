e5875043befa3f54e528d2a8bb96eda555945db3
Stephan Avenwedde <s.avenwedde@gmail.com>
2019-02-24 22:13:03 +0100

    Basic Stack Element: Working on visualization

--- src/Pythonic/elements/basic_stack_window.py
+++ src/Pythonic/elements/basic_stack_window.py
@@ -1,6 +1,6 @@
-from PyQt5.QtWidgets import (QLabel, QWidget, QVBoxLayout, QHBoxLayout, QSizePolicy, QStyleOption, QStyle,
-                                QPushButton, QTextEdit, QMainWindow)
-from PyQt5.QtGui import QPixmap, QPainter, QPen, QFont
+from PyQt5.QtWidgets import (QLabel, QWidget, QVBoxLayout, QHBoxLayout, QSizePolicy,
+        QStyleOption, QStyle, QPushButton, QTextEdit, QListWidgetItem, QListWidget)
+from PyQt5.QtGui import QPixmap, QPainter, QPen, QFont, QColor, QBrush
 from PyQt5.QtCore import Qt, QCoreApplication, pyqtSignal, QPoint, QRect
 from PyQt5.QtCore import QCoreApplication as QC
 from PyQt5.QtCore import QThread
@@ -8,6 +8,16 @@ from elementmaster import alphabet
 import multiprocessing as mp
 from elementeditor import ElementEditor
 import logging, sys, time, traceback
+
+class StackItem(QListWidgetItem):
+
+    def __init__(self, parent, position):
+
+        super().__init__(parent)
+        self.parent = parent
+        self.setAttribute(Qt,WA_DeleteOnClose, True)
+
+        self.setText(position)
 
 class StackWindow(QWidget):
 
@@ -40,13 +50,31 @@ class StackWindow(QWidget):
             #alphabet[self.source[1]]))
         self.elementInfo.setText('test 123')
 
-        self.debugMessage = QTextEdit()
-        self.debugMessage.setReadOnly(True)
-        self.debugMessage.setText('debugMessage')
+        #QListWidgetItem = Stack Element
+
+        self.stack_0 = QListWidgetItem()
+        self.stack_0.setBackground(QBrush(QColor('red')))
+        self.stack_0.setText('Stack 0')
+        self.stack_0.setTextAlignment(Qt.AlignCenter)
+
+        self.stack_1 = QListWidgetItem()
+        self.stack_1.setBackground(QBrush(QColor('green')))
+        self.stack_1.setText('Stack 1')
+
+        self.stack_2 = QListWidgetItem()
+        self.stack_2.setBackground(QBrush(QColor('yellow')))
+        self.stack_2.setText('Stack 2')
+
+        # Will contain the QListWidgetItems
+        self.stackWidget = QListWidget()
+
+        self.stackWidget.insertItem(0, self.stack_0)
+        self.stackWidget.insertItem(0, self.stack_1)
+        self.stackWidget.insertItem(0, self.stack_2)
 
         self.debugWindowLayout = QVBoxLayout()
         self.debugWindowLayout.addWidget(self.elementInfo)
-        self.debugWindowLayout.addWidget(self.debugMessage)
+        self.debugWindowLayout.addWidget(self.stackWidget)
         self.debugWindowLayout.addStretch(1)
         self.debugWindowLayout.addWidget(self.confirm_button)
 
