fa85052927636ec7c720d4b1831087bef66da6ea
Stephan Avenwedde <s.avenwedde@gmail.com>
2019-05-20 12:17:46 +0200

    pythonic_binance: requred version increased

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.whl', 'r') as fh:
 
 setuptools.setup(
         name = 'Pythonic',
-        version = '0.9',
+        version = '0.10',
         author = 'Stephan Avenwedde',
         author_email = 's.avenwedde@gmail.com',
         license = 'GPLv3',
@@ -26,7 +26,7 @@ setuptools.setup(
         install_requires = [
             'PyQt5>=5.6,<=5.8.2',
             'pandas>=0.20.3',
-            'pythonic-binance>=0.7.1'],
+            'pythonic-binance>=0.7.2'],
         classifiers = [
             'Programming Language :: Python :: 3 :: Only',
             'Programming Language :: Python :: 3.5',
