a9963df0abbbce5142fecb336c5932eb491279b4
Stephan Avenwedde <s.avenwedde@gmail.com>
2019-06-20 16:24:13 +0200

    Implemented multiple grids

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.whl', 'r') as fh:
 
 setuptools.setup(
         name = 'Pythonic',
-        version = '0.11',
+        version = '0.12',
         author = 'Stephan Avenwedde',
         author_email = 's.avenwedde@gmail.com',
         license = 'GPLv3',
