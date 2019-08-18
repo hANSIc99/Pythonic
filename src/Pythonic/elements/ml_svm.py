0e62efc8b56b8223097e49ea5fb446094baf7111
Stephan Avenwedde <s.avenwedde@gmail.com>
2019-08-18 21:56:35 +0200

    SVM GUI (train) ready

--- src/Pythonic/elements/ml_svm.py
+++ src/Pythonic/elements/ml_svm.py
@@ -23,8 +23,13 @@ class MLSVM(ElementMaster):
         self.row = row
         self.column = column
 
-        # train_eval, decision_function, gamma_mode, gamma_value, filename, log_state
+        # scale_option, scale_mean, scale_std train_eval, decision_function, gamma_mode, 
+        #    gamma_value, filename, log_state
 
+        
+        scale_option        = 0
+        scale_mean          = True
+        scale_std           = True
         train_eval          = 2
         decision_function   = 0
         gamma_mode          = 0
@@ -32,7 +37,8 @@ class MLSVM(ElementMaster):
         filename            = None
         log_state           = False
 
-        self.config = train_eval, decision_function, gamma_mode, gamma_value, filename, log_state
+        self.config = scale_option, scale_mean, scale_std, train_eval, decision_function, \
+                gamma_mode, gamma_value, filename, log_state
 
         super().__init__(self.row, self.column, QPixmap(self.pixmap_path), True, self.config)
         super().edit_sig.connect(self.edit)
@@ -63,9 +69,36 @@ class MLSVM(ElementMaster):
 
         data split train / eval
         """
-        # train_eval, decision_function, gamma_mode, gamma_value, filename, log_state
-        self.train_eval, self.decision_function, self.gamma_mode, \
-            self.gamma_value, self.filename, self.log_state = self.config
+        self.scale_option, self.scale_mean, self.scale_std, self.train_eval, self.decision_function, \
+                self.gamma_mode, self.gamma_value, self.filename, self.log_state = self.config
+
+        self.scale_label = QLabel()
+        self.scale_label.setText(QC.translate('', 'Scale n_samples ?'))
+        self.scale_list = QComboBox()
+        self.scale_list.addItem('No', QVariant(False))
+        self.scale_list.addItem('Yes', QVariant(True))
+
+        
+        self.scale_center_input_line = QWidget()
+        self.scale_center_input_line_layout = QHBoxLayout(self.scale_center_input_line)
+        self.scale_center_label = QLabel()
+        self.scale_center_label.setText(QC.translate('', 'Center data before scaling?'))
+        self.scale_center_checkbox = QCheckBox()
+        self.scale_center_input_line_layout.addWidget(self.scale_center_label)
+        self.scale_center_input_line_layout.addWidget(self.scale_center_checkbox)
+
+        self.scale_std_input_line = QWidget()
+        self.scale_std_input_line_layout = QHBoxLayout(self.scale_std_input_line)
+        self.scale_std_label = QLabel()
+        self.scale_std_label.setText(QC.translate('', 'Scale data until variance?'))
+        self.scale_std_checkbox = QCheckBox()
+        self.scale_std_input_line_layout.addWidget(self.scale_std_label)
+        self.scale_std_input_line_layout.addWidget(self.scale_std_checkbox)
+
+        self.scale_input_area = QWidget()
+        self.scale_input_area_layout = QVBoxLayout(self.scale_input_area)
+        self.scale_input_area_layout.addWidget(self.scale_center_input_line)
+        self.scale_input_area_layout.addWidget(self.scale_std_input_line)
 
         self.train_test_label = QLabel()
         self.train_test_label.setText(
@@ -94,7 +127,6 @@ class MLSVM(ElementMaster):
         self.gamma_list.addItem('Manual', QVariant('manual'))
 
         self.gamma_input_line = QWidget()
-        #self.gamma_input_line.setMinimumHeight(100)
         self.gamma_input_line_layout = QHBoxLayout(self.gamma_input_line)
         self.gamma_input_txt = QLabel()
         self.gamma_input_txt.setText(QC.translate('', 'Gamma:'))
@@ -114,25 +146,15 @@ class MLSVM(ElementMaster):
 
         
         """
-        self.pass_input_line = QWidget()
-        self.pass_input_txt = QLabel()
-        self.pass_input_txt.setText(QC.translate('','Use input string as URL?'))
-        self.pass_input_check = QCheckBox()
-        self.pass_input_line_layout = QHBoxLayout(self.pass_input_line)
-        self.pass_input_line_layout.addWidget(self.pass_input_txt)
-        self.pass_input_line_layout.addWidget(self.pass_input_check)
-        self.pass_input_line_layout.addStretch(1)
-
-
-        self.url_address_txt = QLabel()
-        self.url_address_txt.setText(QC.translate('', 'URL:'))
-        self.url_address_input = QLineEdit()
-        self.url_address_input.setPlaceholderText(
-                QC.translate('', 'https://www.bitstamp.net/api/ticker/'))
-
+        output: prediction quality
         """
         self.help_text_1 = QLabel()
-        self.help_text_1.setText(QC.translate('', 'GET answer is transformed to Python list object'))
+        self.help_text_1.setText(QC.translate('', 'Expects a tuple (n_samples, n_features) as input.'))
+
+        self.help_text_2 = QLabel()
+        self.help_text_2.setText(QC.translate('', 'Outputs a contigency table in the format:'))
+        self.help_text_3 = QLabel()
+        self.help_text_3.setText(QC.translate('', '{\'TP\': 23, \'FP\': 13, \'FN\':12, \'TN\': 33}'))
 
         
 
@@ -149,16 +171,21 @@ class MLSVM(ElementMaster):
         
         self.ml_svm_edit = ElementEditor(self)
         self.ml_svm_edit.setWindowTitle(QC.translate('', 'Support Vector Machine'))
-        self.ml_svm_edit.setMinimumHeight(500)
+        self.ml_svm_edit.setMinimumHeight(600)
 
         # signals and slots
-        self.gamma_list.currentIndexChanged.connect(self.indexChanged)
+        self.gamma_list.currentIndexChanged.connect(self.gammaIndexChanged)
+        self.scale_list.currentIndexChanged.connect(self.scaledIndexChanged)
         self.confirm_button.clicked.connect(self.ml_svm_edit.closeEvent)
         self.ml_svm_edit.window_closed.connect(self.edit_done)
 
         # load config
         self.loadLastConfig()
 
+        self.conn_rest_layout.addWidget(self.help_text_1)
+        self.conn_rest_layout.addWidget(self.scale_label) # scale: copy = false
+        self.conn_rest_layout.addWidget(self.scale_list)
+        self.conn_rest_layout.addWidget(self.scale_input_area)
         self.conn_rest_layout.addWidget(self.train_test_label)
         self.conn_rest_layout.addWidget(self.train_test_list)
         self.conn_rest_layout.addWidget(self.decision_function_label)
@@ -169,7 +196,8 @@ class MLSVM(ElementMaster):
         self.conn_rest_layout.addWidget(self.filename_text)
         self.conn_rest_layout.addWidget(self.file_button)
         self.conn_rest_layout.addStretch(1)
-        self.conn_rest_layout.addWidget(self.help_text_1)
+        self.conn_rest_layout.addWidget(self.help_text_2)
+        self.conn_rest_layout.addWidget(self.help_text_3)
         self.conn_rest_layout.addWidget(self.log_line)
         self.conn_rest_layout.addWidget(self.confirm_button)
         self.ml_svm_edit.setLayout(self.conn_rest_layout)
@@ -182,15 +210,28 @@ class MLSVM(ElementMaster):
         self.train_test_list.setCurrentIndex(self.train_eval)
         self.decision_function_list.setCurrentIndex(self.decision_function)
         self.gamma_list.setCurrentIndex(self.gamma_mode)
-        self.gamma_input.setText(self.gamma_value)
-        self.indexChanged(self.gamma_mode)
+        self.gamma_input.setText('{}'.format(self.gamma_value))
+        self.gammaIndexChanged(self.gamma_mode)
+        self.scale_list.setCurrentIndex(self.scale_option)
+        self.scaledIndexChanged(self.scale_option)
+        self.scale_center_checkbox.setChecked(self.scale_mean)
+        self.scale_std_checkbox.setChecked(self.scale_std)
         if self.filename:
             self.filename_text.setText(self.filename)
 
-    def indexChanged(self, event):
+    def scaledIndexChanged(self, event):
 
         current_index = event
-        logging.debug('MLSVM::indexChanged() called: {}'.format(event))
+        logging.debug('MLSVM::scaledIndexChanged() called: {}'.format(event))
+        if event == 1 :
+            self.scale_input_area.setVisible(True)
+        else:
+            self.scale_input_area.setVisible(False)
+
+    def gammaIndexChanged(self, event):
+
+        current_index = event
+        logging.debug('MLSVM::gammaIndexChanged() called: {}'.format(event))
         if event == 2 :
             self.gamma_input_line.setVisible(True)
         else:
@@ -211,18 +252,22 @@ class MLSVM(ElementMaster):
     def edit_done(self):
 
         logging.debug('MLSVM::edit_done() called')
+        # scale_option, scale_mean, scale_std train_eval, decision_function, gamma_mode, 
+        #    gamma_value, filename, log_state
 
-        """
-        # pass_input, url, log_state
+        scale_option        = self.scale_list.currentIndex()
+        scale_mean          = self.scale_center_checkbox.isChecked()
+        scale_std           = self.scale_std_checkbox.isChecked()
+        train_eval          = self.train_test_list.currentIndex()
+        decision_function   = self.decision_function_list.currentIndex()
+        gamma_mode          = self.gamma_list.currentIndex()
+        gamma_value         = float(self.gamma_input.text())
+        filename            = self.filename
+        log_state           = self.log_checkbox.isChecked()
 
-        pass_input  = self.pass_input_check.isChecked()
-        url         = self.url_address_input.text()
-        log_state   = self.log_checkbox.isChecked()
-
-        self.config = pass_input, url, log_state
-        logging.debug('########CONFIG: {}'.format(self.config))
-        """
-
+        self.config = scale_option, scale_mean, scale_std, train_eval, decision_function, gamma_mode, \
+                gamma_value, filename, log_state
+        
         self.addFunction(MLSVMFunction)
 
 class MLSVMFunction(Function):
