e847349cd8ac4e2260ecf9ce548a427e895bfdf3
Stephan Avenwedde <s.avenwedde@gmail.com>
2019-09-11 22:24:38 +0200

    Update of logging functionality

--- src/Pythonic/elements/basic_stack.py
+++ src/Pythonic/elements/basic_stack.py
@@ -98,6 +98,7 @@ class ExecStack(ElementMaster):
         self.top_text.setText(QC.translate('', 'Choose file on hard disc for storing stack data:'))
 
         self.filename_text = QLabel()
+        self.filename_text.setWordWrap(True)
         if self.filename:
             self.filename_text.setText(self.filename)
 
