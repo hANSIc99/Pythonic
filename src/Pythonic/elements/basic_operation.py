382eb925b542d231f3746fdda6d0aa7f2c6d6f7f
Stephan Avenwedde <s.avenwedde@gmail.com>
2019-11-13 21:27:48 +0100

    implementing custom editor option

--- src/Pythonic/elements/basic_operation.py
+++ src/Pythonic/elements/basic_operation.py
@@ -2,8 +2,8 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (QVBoxLayout, QHBoxLayout, QLabel, QTextEdit, QWidget,
         QCheckBox, QSpacerItem, QGridLayout, QPushButton, QLineEdit)
 from PyQt5.QtCore import QCoreApplication as QC
-import logging, os, Pythonic
-from Pythonic.elementmaster import ElementMaster
+import logging, os, tempfile, random, subprocess, Pythonic
+from Pythonic.elementmaster import ElementMaster, alphabet
 from Pythonic.elementeditor import ElementEditor
 from Pythonic.record_function import Record, Function
 
@@ -61,7 +61,6 @@ class ExecOp(ElementMaster):
 
         self.custom_editor_checkbox = QCheckBox()
         self.custom_editor_cmd = QLineEdit()
-        self.custom_editor_cmd.setPlaceholderText('tbd')
         self.custom_editor_line = QWidget()
         self.custom_editor_line_layout = QHBoxLayout(self.custom_editor_line)
         self.custom_editor_line_layout.addWidget(self.cmd_line_txt_1)
@@ -117,19 +116,25 @@ class ExecOp(ElementMaster):
         self.op_edit.show()
 
     def toggle_custom_editor(self, event):
-        logging.debug('ExecOp::toggle_custom_editor() called')
         logging.debug('ExecOp::toggle_custom_editor() called {}'.format(event))
+        if event == 2: #custom editor enabled
+            self.code_input.setEnabled(False)
+            self.custom_editor_cmd.setEnabled(True)
+        else:
+            self.code_input.setEnabled(True)
+            self.custom_editor_cmd.setEnabled(False)
+
+
 
     def loadLastConfig(self):
         logging.debug('ExecOp::loadLastConfig() called')
         log_state, code_input, custom_edit_state, cmd = self.config
 
-        if log_state:
-            self.log_checkbox.setChecked(True)
-
+        self.log_checkbox.setChecked(log_state)
+        self.custom_editor_checkbox.setChecked(custom_edit_state)
 
         if code_input:
-            self.code_input.setPlainText(self.config[1])
+            self.code_input.setPlainText(code_input)
         else:
             self.placeholder_1 = QC.translate('',
                     '""" use the variable input to access data from previous elements """')
@@ -138,11 +143,44 @@ class ExecOp(ElementMaster):
             self.placeholder_3 = QC.translate('',
                     '""" set the variable log_txt to adjust the logging text """')
             self.code_input.setPlaceholderText(self.placeholder_1 + '\r\n\r\n' +
-                                               'print(input)\r\n\r\n' +
-                                               self.placeholder_2 +
-                                               '\r\n\r\n' + 'output = 5\r\n\r\n' + 
-                                               self.placeholder_3 + '\r\n\r\n' +
-                                               'log_txt = "debug text"')
+                                           'print(input)\r\n\r\n' +
+                                           self.placeholder_2 +
+                                           '\r\n\r\n' + 'output = 5\r\n\r\n' + 
+                                           self.placeholder_3 + '\r\n\r\n' +
+                                           'log_txt = "debug text"')
+
+        if cmd:
+            self.custom_editor_cmd.setText(cmd)
+        else:
+            self.custom_editor_cmd.setPlaceholderText('tbd')
+
+        if custom_edit_state:
+            self.code_input.setEnabled(False)
+            self.custom_editor_cmd.setEnabled(True)
+            self.openCustomEditor(cmd, code_input)
+        else:
+            self.code_input.setEnabled(True)
+            self.custom_editor_cmd.setEnabled(False)
+
+        
+    def openCustomEditor(self, cmd, code_input):
+        logging.debug('ExecOp::openCustomEditor() called')
+        filename = '{}_{}_{}'.format(self.row, alphabet[self.column], int(random.random() * 1e7))
+        filename = os.path.join(tempfile.gettempdir(), filename)
+        logging.debug('ExecOp::openCustomEditor() filename: {}'.format(filename))
+
+        try:
+            # create new file
+            with open(filename, 'w') as f:
+                f.write(code_input)
+        except Exception as e:
+            # not writeable?
+            return e
+        # subprocess.call('vim', shell=True)
+        os.system('vim')
+
+        logging.debug('ExecOp::openCustomEditor() subprocess called')
+        #os.remove(filename)
 
     def edit_done(self):
         logging.debug('edit_done() called ExecOp' )
@@ -152,8 +190,12 @@ class ExecOp(ElementMaster):
         else:
             code_input = self.code_input.toPlainText()
 
-        custom_edit_state = False
-        cmd = None
+        if self.custom_editor_cmd.text() == '':
+            cmd = None
+        else:
+            cmd = self.custom_editor_cmd.text()
+
+        custom_edit_state = self.custom_editor_checkbox.isChecked()
 
         self.config = (self.log_checkbox.isChecked(), code_input, custom_edit_state, cmd)
         self.addFunction(OperationFunction)
@@ -174,7 +216,7 @@ class OperationFunction(Function):
         exec_string = 'input = record\r\n'
         exec_string += 'output = record\r\n'
 
-        #logging.warning('Exec-String:\r\n{}'.format(exec_string))
+                #logging.warning('Exec-String:\r\n{}'.format(exec_string))
         
         if code_input:
             #logging.warning('Appending user specific code')
@@ -190,7 +232,7 @@ class OperationFunction(Function):
         else:
             log_txt = '{{BASIC OPERATION}}        {}'.format(proc_dict['output'])
 
-
+        output = filename
         result = Record(self.getPos(), (self.row+1, self.column), output, log=log_state, log_txt=log_txt)
                 
         return result
