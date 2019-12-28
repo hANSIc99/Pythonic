08a87af751303ad1f5101a23f35f8aa6928ffa27
Stephan Avenwedde <s.avenwedde@gmail.com>
2019-12-28 12:31:06 +0100

    dev webassembly

--- src/PythonicWeb/main.cpp
+++ src/PythonicWeb/main.cpp
@@ -2,6 +2,8 @@
 
 #include <QApplication>
 
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
--- src/PythonicWeb/mainwindow.h
+++ src/PythonicWeb/mainwindow.h
@@ -2,20 +2,23 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QWidget>
+#include <QPushButton>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    explicit MainWindow(QWidget *parent = nullptr,
+                        Qt::WindowFlags flags = 0);
 
+private slots:
+    void handleButton();
 private:
-    Ui::MainWindow *ui;
+
+    QPushButton *m_button;
 };
 #endif // MAINWINDOW_H
