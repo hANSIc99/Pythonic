6cf578a7a0f57d13ca71c64b8f74eda59237d5f1
Stephan Avenwedde <s.avenwedde@gmail.com>
2019-12-28 12:31:06 +0100

    dev webassembly

--- src/PythonicWeb/PythonicWeb.pro
+++ src/PythonicWeb/PythonicWeb.pro
@@ -1,4 +1,4 @@
-QT       += core gui
+#QT       += core gui
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -22,8 +22,7 @@ SOURCES += \
 HEADERS += \
     mainwindow.h
 
-FORMS += \
-    mainwindow.ui
+FORMS +=
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
--- src/PythonicWeb/mainwindow.cpp
+++ src/PythonicWeb/mainwindow.cpp
@@ -1,15 +1,29 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include <QDebug>
+#include <QString>
+#include <QDir>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent, Qt::WindowFlags flags)
+    : QMainWindow(parent, flags)
+
 {
-    ui->setupUi(this);
+    this->resize(500, 300);
+    m_button = new QPushButton("My CPP Button", this);
+    m_button->setGeometry(QRect(QPoint(100, 100), QSize(200, 50)));
+    connect(m_button, SIGNAL(released()), this, SLOT(handleButton()) );
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
 
+void MainWindow::handleButton(){
+    qDebug() << "Button pressed";
+    QString path("/home/stephan/Dokumente/Pythonic/src/PythonicWeb/testdir/");
+    QDir dir; // Initialize to the desired dir if 'path' is relative
+              // By default the program's working directory "." is used.
+
+    // We create the directory if needed
+    if (!dir.exists(path))
+        dir.mkpath(path); // You can check the success if needed
+
+    QFile file(path + "NewFile.kml");
+    file.open(QIODevice::WriteOnly); // Or QIODevice::ReadWrite
+}
