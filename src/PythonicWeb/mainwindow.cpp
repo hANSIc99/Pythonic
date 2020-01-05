ebfcbe5f24e9a6f0c9a7c490b595a28c4577fcf9
Stephan Avenwedde <s.avenwedde@gmail.com>
2020-01-05 14:26:05 +0100

    dev websockets demo

--- src/PythonicWeb/mainwindow.cpp
+++ src/PythonicWeb/mainwindow.cpp
@@ -6,23 +6,40 @@
 #include <QFileDialog>
 #include <QHttpPart>
 #include <QObject>
-#include <QScopedPointer>
+
 
 MainWindow::MainWindow(QWidget *parent, Qt::WindowFlags flags)
     : QMainWindow(parent, flags)
 
 {
     this->resize(500, 300);
-    m_button = new QPushButton("My CPP Button", this);
-    m_button->setGeometry(QRect(QPoint(100, 100), QSize(200, 50)));
-    connect(m_button, SIGNAL(released()), this, SLOT(handleButton()) );
-
     net_mgr = new QNetworkAccessManager(this);
     connect(net_mgr, SIGNAL(finished(QNetworkReply*)), this, SLOT(netFinished(QNetworkReply*)));
 
-    m_open_file_button = new QPushButton("Open Grid", this);
-    m_open_file_button->setGeometry((QRect(QPoint(100, 150), QSize(200, 50))));
+    /*
+     *  STANDARD QUERY
+     */
+
+    m_std_query_button = new QPushButton("Standard Query", this);
+    m_std_query_button->setGeometry(QRect(QPoint(30, 30), QSize(200, 50)));
+    connect(m_std_query_button, SIGNAL(released()), this, SLOT(handleButton()) );
+
+
+    /*
+     * UPLOAD FILE BUTTON
+     */
+
+    m_open_file_button = new QPushButton("Upload File", this);
+    m_open_file_button->setGeometry((QRect(QPoint(30, 100), QSize(200, 50))));
     connect(m_open_file_button, SIGNAL(released()), this, SLOT(openFileBrowser()) );
+
+    /*
+     * WEBSOCKET CONNECT BUTTON
+     */
+
+    m_websocket_connect_button = new QPushButton("Connect Websocket", this);
+    m_websocket_connect_button->setGeometry((QRect(QPoint(30, 170), QSize(200, 50))));
+    connect(m_websocket_connect_button, SIGNAL(released()), this, SLOT(connectWebSocket()) );
 }
 
 
@@ -41,28 +58,30 @@ void MainWindow::openFileBrowser(){
     qDebug() << "MainWindow::openFileBrowser() called";
     QString s_homePath = QDir::homePath();
     //QFileDialog::getOpenFileContent
-    auto fileOpenCompleted = [this](const QString &fileName, const QByteArray &fileContent) {
-        if (fileName.isEmpty()) {
+    auto fileOpenCompleted = [this](const QString &filePath, const QByteArray &fileContent) {
+        if (filePath.isEmpty()) {
             qDebug() << "No file was selected";
         } else {
             qDebug() << "Size of file: " << fileContent.size() / 1000 << "kb";
-            qDebug() << "Filename: " << fileName;
-            QHttpPart fileDataPart;
-            // BAUSTELLE SMART POINTER
-            QHttpMultiPart *multipart = new QHttpMultiPart(QHttpMultiPart::FormDataType);
+            qDebug() << "Selected file: " << filePath;
 
-            fileDataPart.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=\"file\"; filename=\"myfilename\""));
+
+
+            QFileInfo fileName(filePath);
+            QString content_header = QString("form-data; name=\"file\"; filename=\"%1\"").arg(fileName.fileName());
+
+            QHttpPart fileDataPart;
+            fileDataPart.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant(content_header));
             fileDataPart.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("image/jpeg"));
             fileDataPart.setBody(fileContent);
 
             QUrl url("http://localhost:5000/test");
             QNetworkRequest qnet_req(url);
 
+            QHttpMultiPart *multipart = new QHttpMultiPart(QHttpMultiPart::FormDataType);
             multipart->append(fileDataPart);
 
             QNetworkReply *reply = net_mgr->post(qnet_req, multipart);
-            std::unique_
-            //multipart->setParent(dynamic_cast<QObject*>(reply));
         }
     };
 
@@ -79,6 +98,12 @@ void MainWindow::fileOpenComplete(const QString &fileName, const QByteArray &dat
     qDebug() << "MainWindow::fileOpenComplete() called";
     qDebug() << "Filename: " << fileName;
     qDebug() << "Size: " << data.size();
+}
+
+void MainWindow::connectWebSocket()
+{
+    qDebug() << "MainWindow::connectWebSocket() called";
+
 }
 
 #if 0
