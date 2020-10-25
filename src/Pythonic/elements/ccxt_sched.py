0f4cf8086ef84eba44077eb469749f8b8282fa00
Stephan <stephan@localhost.localdomain>
2020-10-25 21:17:37 +0100

    files prepared for ccxt scheduler

---
+++ src/Pythonic/elements/ccxt_sched.py
@@ -0,0 +1,10 @@
+from PyQt5.QtCore import QVariant
+from PyQt5.QtGui import  QIntValidator
+from PyQt5.QtWidgets import (QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton,
+        QLabel, QWidget, QComboBox, QCheckBox)
+from PyQt5.QtCore import QCoreApplication as QC
+import logging
+from Pythonic.elementeditor import ElementEditor
+from Pythonic.elementmaster import ElementMaster
+from Pythonic.elements.binance_sched_func import BinanceScheduler
+
---
+++ src/Pythonic/elements/ccxt_sched_func.py
@@ -0,0 +1,7 @@
+from pythonic_binance.client import Client
+from time import sleep
+import datetime
+from Pythonic.record_function import Record, Function
+
+
+# https://github.com/ccxt/ccxt/wiki/Manual
