125b571c91007a3c70e7ef1356f392249fafc035
Stephan <stephan@localhost.localdomain>
2021-04-18 21:05:31 +0200

    dev commit

--- src/Pythonic/executables/telegram.py
+++ src/Pythonic/executables/telegram.py
@@ -29,20 +29,85 @@ class Element(Function):
         #                                   #
         #####################################
 
-
         cmd = None
-        cnt = 0
+        specificConfig = self.config.get('SpecificConfig')
 
-        #####################################
-        #                                   #
-        #     Start of the infinite loop    #
-        #                                   #
-        #####################################
+        # Set default mode if SpecificConfig is not defined
+        # This is the case if the element was created on the working area
+        # but the configuration was never opened
 
-        # The example executes an infinite loop till it's receives a stop command
-        while(True):
+        if not specificConfig:
 
-            # Example code: Do something
+            recordDone = Record(None, message='Config missing')
+            self.return_queue.put(recordDone)
+            return
+
+
+        token = next(attr['Data'] for attr in specificConfig if attr['Name'] == 'Token')
+
+        if not token:
+            recordDone = Record(None, message='Token missing')
+            self.return_queue.put(recordDone)
+            return
+
+
+        updater = Updater(token='1323666957:AAH6rIDQJty0ixLc9fyrLQIohxks_y6wwho', use_context=True)
+
+        dispatcher = updater.dispatcher
+
+        def start(update, context):
+            location_keyboard = KeyboardButton(text="send_location", request_location=True)
+            #custom_keyboard = [[ location_keyboard ]]
+            #reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+            #context.bot.send_message(chat_id=update.effective_chat.id, 
+            #                text="Would you mind sharing your location and contact with me?", 
+            #                reply_markup=reply_markup)
+            context.bot.send_message(chat_id=update.effective_chat.id, 
+                            text="Hello from bot")
+
+
+        def echo(update, context):
+            context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+        def caps(update, context):
+            text_caps = ''.join(context.args).upper()
+            context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+        def inline_caps(update, context):
+            query = update.inline_query.query
+            if not query:
+                return
+            results = list()
+            results.append(
+                    InlineQueryResultArticle(
+                        id=query.upper(),
+                        title='Caps',
+                        input_message_content=InputTextMessageContent(query.upper())
+                    )
+            )
+            context.bot.answer_inline_query(update.inline_query.id, results)
+
+        inline_caps_handler = InlineQueryHandler(inline_caps)
+
+        def unknown(update, context):
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Sorry, I didn\'t understand that command.')
+
+
+        start_handler = CommandHandler('start', start)
+        echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+        caps_handler = CommandHandler('caps', caps)
+        unknown_handler = MessageHandler(Filters.command, unknown)
+        #echo_handler = MessageHandler(Filters.location & (~Filters.command), echo)
+
+        dispatcher.add_handler(start_handler)
+        #dispatcher.add_handler(echo_handler)
+        #dispatcher.add_handler(caps_handler)
+        dispatcher.add_handler(inline_caps_handler)
+        dispatcher.add_handler(unknown_handler) # muss als letztes hinzugefügt werden
+
+        updater.start_polling()
+
+        while(updater.running):
 
             try:
                 # Block for 1 second and wait for incoming commands 
@@ -50,23 +115,6 @@ class Element(Function):
             except queue.Empty:
                 pass
 
-            if isinstance(cmd, ProcCMD):
-                if cmd.bStop:
-                    # Stop command received, exit
-                    return
-                else:
-                    # Example Code: Send number of received data packets to GUI
-                    cnt+=1
-                    guitext = GuiCMD('Data received: {}'.format(cnt))
-                    self.return_queue.put(guitext)
-                    cmd.data += 1
-                    
-                    # Example Code: Increment received data by one and forward it to subsequent elements
-                    recordDone = Record(cmd.data, 'Sending value of cnt: {}'.format(cmd.data))     
-                    self.return_queue.put(recordDone)
-                    
-                    cmd = None
-
-
-
-
+            if isinstance(cmd, ProcCMD) and cmd.bStop:
+                # Stop command received, exit
+                updater.stop()
