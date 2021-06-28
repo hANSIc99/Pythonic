c2210e253d2c415fb279a384d0234ebfd6a5ff95
stephan <stephan@localhost.localdomain>
2021-06-28 16:49:01 +0200

    dev commit

--- src/Pythonic/build.sh
+++ src/Pythonic/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd "${0%/*}"
 
-source ~/Dokumente/emsdk/emsdk_env.sh # Version 1.39.8
+source ~/emsdk/emsdk_env.sh # Version 1.39.8
 ~/Qt/5.15.1/wasm_32/bin/qmake
 # Set WASM define
 #make DEFINES="-DQT_NO_DEBUG -DQT_WIDGETS_LIB -DQT_GUI_LIB -DQT_WEBSOCKETS_LIB -DQT_NETWORK_LIB -DQT_CORE_LIB -DWASM"
@@ -12,3 +12,4 @@ cp *.html public_html/static/
 cp *.wasm public_html/static/
 cp *.js public_html/static/
 cp *.data public_html/static/
+cp PythonicWeb.html public_html/templates/
