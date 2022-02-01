cc45722d4ebf7f666107727587ed1bc48f49a995
Stephan Avenwedde <s.avenwedde@gmail.com>
2022-02-01 16:56:03 +0100

    dev commit

--- examples/logging_and_report/generic_pipe_0e7b8360.py
+++ examples/logging_and_report/generic_pipe_0e7b8360.py
@@ -13,10 +13,7 @@ class Element(Function):
 
     def execute(self):
 
-
-
-
-        # Raspberry Pi Only
+        # Raspberry Pi only
         #cpu_temp = os.popen("vcgencmd measure_temp").readline().replace("temp=", "").replace("'C\n", "")
         #cpu_temp = float(cpu_temp)
 
@@ -26,14 +23,5 @@ class Element(Function):
 
         output = 'INSERT INTO my_table VALUES ({}, {})'.format(int(time.time()), cpu_temp)
         
-        
-        #########################################
-        #                                       #
-        #    The execution exits immediately    #
-        #    after providing output data        #
-        #                                       #
-        #########################################
-
-        #recordDone = Record('hello123', 'Telegram message send')  
         recordDone = Record(output)     
         self.return_queue.put(recordDone)
